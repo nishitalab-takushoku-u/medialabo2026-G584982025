2126ecdeb2f4af804d887f2a5dc939e0ba5f21fb
Yamazaki Yuushin <258498@st.takushoku-u.ac.jp>
2026-06-18 15:41:52 +0900

    第3回授業中Take2

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,12 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+
+for(let a of campus.buildingD) {
+	console.log(a);
+}
+
+for (let b of gakka) {
+	console.log(b.name);
+}
